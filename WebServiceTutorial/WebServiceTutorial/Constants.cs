4532e893aabf3422e41c5262cee412da9e4f7dc9
Abel Pech <abel.pech@gmail.com>
2020-04-30 22:54:06 -0700

    Add project files.

---
+++ WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WebServiceTutorial
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "9ee5f4caf205a4383d5672615c892ef8";
+    }
+}
